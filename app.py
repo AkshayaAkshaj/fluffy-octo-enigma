9487750d3c6c1e821917ebc044362a21663f11f7
AkshayaAkshaj <87810890+AkshayaAkshaj@users.noreply.github.com>
2023-11-24 18:21:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template , redirect, url_for, send_file
+from twilio.rest import Client
+
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+	return render_template('home.html')
+
+if __name__ == "__main__":
+	app.run()
+
+# Call the app.run()
+
+
+
